a44bb71c4b487e15775bf045656b6ff1e8a0f249
AlissCava <cavalleraalissa@gmail.com>
2023-11-28 14:49:46 +0100

    italian js

---
+++ script.js
@@ -0,0 +1,29 @@
+// Otteniamo il riferimento al contenitore della griglia nel DOM
+
+
+
+// Funzione per generare la griglia quando l'utente clicca sul pulsante "Genera Griglia"
+
+    // Puliamo il contenuto del contenitore della griglia
+
+    // Cicliamo da 1 a 100 per creare le celle della griglia
+
+        // Creiamo un elemento div per rappresentare una cella
+
+        // Assegniamo il numero progressivo alla cella
+
+        // Aggiungiamo le classi CSS necessarie alla cella
+
+        // Aggiungiamo un listener per gestire il clic sulla cella
+
+        // Aggiungiamo la cella al contenitore della griglia
+
+
+
+// Funzione chiamata quando l'utente clicca su una cella
+
+    // Stampiamo il numero della cella cliccata nella console
+
+    // Selezioniamo la cella cliccata utilizzando il selettore CSS e le classi
+    
+    // Aggiungiamo una classe alla cella cliccata per cambiare il suo stile
