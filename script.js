791151e8085dc7327081faff86c98719f6ded169
AlissCava <cavalleraalissa@gmail.com>
2023-11-28 15:25:45 +0100

    genera l agriglia

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
 // Otteniamo il riferimento al contenitore della griglia nel DOM
+const gridContainer = document.getElementById('grid-container');
 
 
 
 // Funzione per generare la griglia quando l'utente clicca sul pulsante "Genera Griglia"
-
+function generateGrid() {
     // Puliamo il contenuto del contenitore della griglia
-
+    gridContainer.innerHTML = '';
+  
     // Cicliamo da 1 a 100 per creare le celle della griglia
-
-        // Creiamo un elemento div per rappresentare una cella
-
-        // Assegniamo il numero progressivo alla cella
-
-        // Aggiungiamo le classi CSS necessarie alla cella
-
-        // Aggiungiamo un listener per gestire il clic sulla cella
-
-        // Aggiungiamo la cella al contenitore della griglia
-
-
-
-// Funzione chiamata quando l'utente clicca su una cella
-
-    // Stampiamo il numero della cella cliccata nella console
-
-    // Selezioniamo la cella cliccata utilizzando il selettore CSS e le classi
-    
-    // Aggiungiamo una classe alla cella cliccata per cambiare il suo stile
+    for (let i = 1; i <= 100; i++) {
+      // Creiamo un elemento div per rappresentare una cella
+      const cell = document.createElement('div');
+  
+      // Assegniamo il numero progressivo alla cella
+      cell.textContent = i;
+  
+      // Aggiungiamo le classi CSS necessarie alla cella
+      cell.classList.add('cell', `cell-${i}`); // Aggiunta della classe specifica alla cella
+  
+      // Aggiungiamo un listener per gestire il clic sulla cella
+      cell.addEventListener('click', () => handleClick(i));
+  
+      // Aggiungiamo la cella al contenitore della griglia
+      gridContainer.appendChild(cell);
+    }
+  }
